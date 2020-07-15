be860bf20aee59e717108305243edbe511fa977a
lbb4511 <lbb4511@qq.com>
2020-07-15 17:21:25 +0800

    docsify

---
+++ docs/_docsify.js
@@ -0,0 +1,15 @@
+window.$docsify = {
+  name: "Lua教程",
+  repo: "lbb4511/lua",
+  maxLevel: 2,
+  subMaxLevel: 1,
+  // coverpage: true,
+  onlyCover: true,
+  loadSidebar: true,
+  search: {
+    maxAge: 86400000, // 过期时间，单位毫秒，默认一天
+    placeholder: '搜索',
+    noData: '找不到结果',
+    depth: 2
+  },
+};
